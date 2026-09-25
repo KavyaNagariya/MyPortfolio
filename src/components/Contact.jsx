5786592b5bf634eec597ee00155aa54294d9fa6c
KavyaNagariya <mehukavyanagariya@gmail.com>
2026-09-25 14:53:18 +0530

    resume updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
           </svg>
           Buy Me A Coffee ↗
         </a>
-        <a href="https://drive.google.com/file/d/1b4SPwX_GVBPO221FWvmOTIXvbLhZtSwW/view?usp=sharing" target="_blank" rel="noreferrer" className="pill">
+        <a href="https://drive.google.com/file/d/1p9mb1-S8KAYYWFiytDpZcMuM_qT8GnZ1/view?usp=sharing" target="_blank" rel="noreferrer" className="pill">
           <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none">
             <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
             <polyline points="14 2 14 8 20 8"></polyline>
